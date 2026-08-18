54978df2114abf22019493635abdeb486a98adb4
sfaridm <shahussainfaridm@gmail.com>
2026-08-18 14:20:45 +0530

    Fix EC2 module dependencies

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,11 +1,8 @@
 resource "aws_instance" "MyEC2" {
-    ami = var.ami_id
-    instance_type = var.instancetype
-    subnet_id = aws_subnet.mypublic1.id
-    vpc_security_group_ids = ["aws_security_group."]
-    tags = {
-        Name = "Yamahaec2"
-    }
-  
-}
+  ami           = var.ami_id
+  instance_type = var.instancetype
 
+  subnet_id = var.subnet_id
+
+  vpc_security_group_ids = [var.security_group_id]
+}
